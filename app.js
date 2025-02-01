65ee7de0bfb4175ff6b8fb274fb0125daad63102
uche <aborkelvin@gmail.com>
2025-02-01 22:06:24 +0100

    Fix cors error

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { userRouter } = require("./routes/user.route");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //Routes
 app.use("/users", userRouter);
